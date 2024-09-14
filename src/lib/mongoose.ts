a96f454656505b5a4dc374b3edff340c39ac5275
Thomas Crockett <thomas.crockett24@gmail.com>
2024-09-14 14:23:33 -0600

    update mongoose.ts

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -3,11 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/sveltekit_db';
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/wcsdb';
 
 export async function connectToDatabase() {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL, {
+        authSource: 'admin',
+        user: 'admin', 
+        pass: 'password',
+        localPort: 27017,
+        dbName: 'wcsdb',
+        family: 4,
+        hints: 'String',
+        localAddress: '',
+        lookup: 'dns.lookup'
+    });
     console.log('Connected to MongoDB');
   }
 }
